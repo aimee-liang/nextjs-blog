e2c397bd93ee7cb9ebf9701ef0b4296e8a892bd3
aimee-liang <aimeeeliang@gmail.com>
2021-02-05 21:20:29 -0500

    Created logo

--- basic-ecommerce-store/src/App.js
+++ basic-ecommerce-store/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Logo from "Logo"
 
 function App() {
   return (
     <>
+    <Logo/>
     </>
   );
 }
---
+++ basic-ecommerce-store/src/Logo.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Logo = () => {
+    return (
+        <img src="/Jewels-logo.png" alt="logo" />
+    )
+}
+
+export default Logo

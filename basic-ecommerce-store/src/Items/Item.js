4c4363d79062c64fafd7c6b5059e5a9b9785bf04
aimee-liang <aimeeeliang@gmail.com>
2021-02-05 21:39:35 -0500

    Created Items folder with Item and Stock

---
+++ basic-ecommerce-store/src/Items/Item.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Item = () => {
+    return (
+        <>
+            <div className="item">
+                <h2>Jewelry Piece Name</h2>
+                <img src="" alt=""/>
+                <h5>$Price</h5>
+            </div>
+        </>
+    )
+}
+
+export default Item
---
+++ basic-ecommerce-store/src/Items/Stock.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Item from "./Item.js"
+
+const Stock = () => {
+    return (
+        <>
+            <div className="stock-page">
+                <Item/>
+            </div>
+        </>
+    )
+}
+
+export default Stock

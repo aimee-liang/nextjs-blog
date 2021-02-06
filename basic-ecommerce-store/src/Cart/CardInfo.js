80854b968988e7951ee2825c40dd114e77be6e9f
aimee-liang <aimeeeliang@gmail.com>
2021-02-05 23:59:16 -0500

    Using setState for fetching api

--- basic-ecommerce-store/src/Cart/CardInfo.js
+++ basic-ecommerce-store/src/Cart/CardInfo.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useQuery} from "react-query"
 
 const CardInfo = () => {
+
+    const [stripe, setStripe] = useState(false) 
+    const [payment, setPayment] = useState([])
+    
     return(
         <>
+            {/* fetch api here */}
         </>
     )
 }

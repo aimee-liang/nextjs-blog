daa80086c6baa1ca85c04c38b14c680b02cf6ae2
aimee-liang <aimeeeliang@gmail.com>
2021-02-07 16:56:04 -0500

    Imported Cart to Stock page, added pseudocode to Cart.js

--- basic-ecommerce-store/src/Cart/Cart.js
+++ basic-ecommerce-store/src/Cart/Cart.js
@@ -4,8 +4,10 @@ import CardInfo from "./CardInfo"
 const Cart = () => {
     return (
         <div className="cart">
+            <h2>Your Cart</h2>
+            {/* if there is nothing in the cart, display "Your cart is empty" */}
             {/* calculate total */}
-            
+
             <CardInfo/>
         </div>
     )
--- basic-ecommerce-store/src/Items/Stock.js
+++ basic-ecommerce-store/src/Items/Stock.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Item from "./Item.js"
+import Cart from "../Cart/Cart"
 
 const Stock = () => {
     return (

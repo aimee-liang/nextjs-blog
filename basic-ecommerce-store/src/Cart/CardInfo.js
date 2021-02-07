d7febc95e0fdb48730c3c1b6b47bc71ade36ff16
aimee-liang <aimeeeliang@gmail.com>
2021-02-06 20:48:01 -0500

    Set up useEffect to fetch api

--- basic-ecommerce-store/src/Cart/CardInfo.js
+++ basic-ecommerce-store/src/Cart/CardInfo.js
@@ -6,13 +6,14 @@ const CardInfo = () => {
 
     const [payment, setPayment] = useState([])
 
-    React.useEffect(function fetchStripe(){
+    React.useEffect(function effectFunction(){
         async function fetchStripe() {
             const response = await fetch("https://api.stripe.com")
             const json = await response.json()
-
+            setPayment(json.data)
         }
-    }
+        fetchStripe()
+    }, [])
 
     
     return(

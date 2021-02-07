9bbb2cb63c5a52986b41d9e652c97bbe88ea268f
aimee-liang <aimeeeliang@gmail.com>
2021-02-06 20:46:40 -0500

    Refactoring CardInfo

--- basic-ecommerce-store/src/Cart/CardInfo.js
+++ basic-ecommerce-store/src/Cart/CardInfo.js
@@ -2,18 +2,22 @@ import React, {useState} from 'react'
 import {useQuery} from "react-query"
 
 
-const fetchStripe = async () => {
-    await (await fetch ("https://api.stripe.com")).json()
-}
 const CardInfo = () => {
 
-    const [stripe, setStripe] = useState(false) 
     const [payment, setPayment] = useState([])
+
+    React.useEffect(function fetchStripe(){
+        async function fetchStripe() {
+            const response = await fetch("https://api.stripe.com")
+            const json = await response.json()
+
+        }
+    }
+
     
     return(
         <>
             {/* fetch api here */}
-
         </>
     )
 }

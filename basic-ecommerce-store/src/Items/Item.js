cc820aff4d2d89d3e650e31873b6097779f32bc3
aimee-liang <aimeeeliang@gmail.com>
2021-02-06 21:21:13 -0500

    Created random price generator for now

--- basic-ecommerce-store/src/Items/Item.js
+++ basic-ecommerce-store/src/Items/Item.js
@@ -1,12 +1,15 @@
 import React from "react"
 
 const Item = () => {
+
+    const randomPriceGenerator = Math.floor(Math.random() * 100) + 1
+
     return (
         <>
             <div className="item">
                 <h2>Jewelry Piece Name</h2>
                 <img src="" alt=""/>
-                <h5>$Price</h5>
+                <h5>${randomPriceGenerator}</h5>
             </div>
         </>
     )

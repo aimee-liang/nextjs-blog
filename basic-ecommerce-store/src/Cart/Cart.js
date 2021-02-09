1cbc3bc787f071b6b4ff0ca2a1872306c1d4bfc8
aimee-liang <aimeeeliang@gmail.com>
2021-02-09 17:54:26 -0500

    Created and imported CartItem.js

--- basic-ecommerce-store/src/Cart/Cart.js
+++ basic-ecommerce-store/src/Cart/Cart.js
@@ -1,13 +1,16 @@
 import React from "react"
 import CardInfo from "./CardInfo"
+import CartItem from "./CartItem"
 
 const Cart = (props) => {
     return (
         <div className="cart">
             <h2>Your Cart</h2>
-            {/* if there is nothing in the cart, display "Your cart is empty" */}
             {cartTotal.length ? <p>Your cart is empty</p> : null}
             {/* display all items */}
+            {cartTotal.map(item => {
+                <
+            })}
                 {/* and possibly increase total quantity */}
             {/* calculate total */}
 

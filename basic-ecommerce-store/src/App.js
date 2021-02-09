5f6bf36b544dc9fd124ffd0423b5705f9a4be930
aimee-liang <aimeeeliang@gmail.com>
2021-02-09 17:48:07 -0500

    Corrected total to Cart total in Cart.js

--- basic-ecommerce-store/src/App.js
+++ basic-ecommerce-store/src/App.js
@@ -14,7 +14,7 @@ const App = () => {
     <>
       <Logo/>
       <Stock updateTotal={updateTotal}/>
-      <Cart total={setTotal}/>
+      <Cart cartTotal={setTotal}/>
     </>
   );
 }
--- basic-ecommerce-store/src/Cart/Cart.js
+++ basic-ecommerce-store/src/Cart/Cart.js
@@ -6,7 +6,7 @@ const Cart = (props) => {
         <div className="cart">
             <h2>Your Cart</h2>
             {/* if there is nothing in the cart, display "Your cart is empty" */}
-                {/* Cart.length === 0 ? <p>"Your cart is empty"</p> : null */}
+            {cartTotal.length ? <p>Your cart is empty</p> : null}
             {/* display all items */}
                 {/* and possibly increase total quantity */}
             {/* calculate total */}

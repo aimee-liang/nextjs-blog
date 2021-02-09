64372ac20a858b357527a0ec3aa0581e639b00a2
aimee-liang <aimeeeliang@gmail.com>
2021-02-09 18:27:22 -0500

    Div for cart-item

--- basic-ecommerce-store/src/Cart/CartItem.js
+++ basic-ecommerce-store/src/Cart/CartItem.js
@@ -1,8 +1,13 @@
 import React from "react"
+import Item from "../Items/Item"
 
-const CartItem = () => {
+const CartItem = (props) => {
     return(
         <>
+            <div className = "cart-item">
+                <h3>{props.item.name}</h3>
+                <h4>{props.item.price}</h4>
+            </div>
         </>
     )
 }

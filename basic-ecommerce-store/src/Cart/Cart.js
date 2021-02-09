bb73dce9140dea9acc49801c0ab3b0380cd564cc
aimee-liang <aimeeeliang@gmail.com>
2021-02-09 18:13:55 -0500

    Corrected cartTotal and mapped through each item in cart

--- basic-ecommerce-store/src/Cart/Cart.js
+++ basic-ecommerce-store/src/Cart/Cart.js
@@ -6,10 +6,13 @@ const Cart = (props) => {
     return (
         <div className="cart">
             <h2>Your Cart</h2>
-            {cartTotal.length ? <p>Your cart is empty</p> : null}
+            {props.cartTotal.length ? <p>Your cart is empty</p> : null}
             {/* display all items */}
-            {cartTotal.map(item => {
-                <
+            {props.cartTotal.map((item, index) => {
+                <CartItem
+                    key = {index}
+                    item = {item}
+                    />
             })}
                 {/* and possibly increase total quantity */}
             {/* calculate total */}

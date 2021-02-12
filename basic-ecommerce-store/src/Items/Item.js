9d984f1a773a81c3783cb71b5daac725a864951b
aimee-liang <aimeeeliang@gmail.com>
2021-02-12 15:22:39 -0500

    randomPriceGenerator()

--- basic-ecommerce-store/src/Items/Item.js
+++ basic-ecommerce-store/src/Items/Item.js
@@ -3,17 +3,15 @@ import Button from "@material-ui/core/Button"
 
 const Item = (props) => {
 
-    const randomPriceGenerator = Math.floor(Math.random() * 100) + 1
+    const randomPriceGenerator = () => Math.floor(Math.random() * 100) + 1
 
     return (
-        <>
-            <div className="item">
-                <h2>Jewelry Piece Name</h2>
-                <img src="" alt=""/>
-                <h5>${randomPriceGenerator}</h5>
-                <Button onClick={props.localUpdate}>Click to purchase</Button>
-            </div>
-        </>
+        <div className="item">
+            <h2>Jewelry Piece Name</h2>
+            <img src="" alt=""/>
+            <h5>${randomPriceGenerator()}</h5>
+            <Button onClick={props.localUpdate}>Click to purchase</Button>
+        </div>
     )
 }
 

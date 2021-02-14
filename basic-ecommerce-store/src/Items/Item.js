91c95900f0168c6b9df930cbdbac689de4ec8396
aimee-liang <aimeeeliang@gmail.com>
2021-02-13 22:08:31 -0500

    four items

--- basic-ecommerce-store/src/Items/Item.js
+++ basic-ecommerce-store/src/Items/Item.js
@@ -6,12 +6,35 @@ const Item = (props) => {
     const randomPriceGenerator = () => Math.floor(Math.random() * 100) + 1
 
     return (
-        <div className="item">
-            <h3>Jewelry Piece Name</h3>
-            <img src="" alt=""/>
-            <h5>${randomPriceGenerator()}</h5>
-            <Button variant="contained" onClick={props.localUpdate}>Click to purchase</Button>
-        </div>
+        <>
+            <div className="item">
+                <h3>Jewelry Piece Name</h3>
+                <img src="" alt=""/>
+                <h5>${randomPriceGenerator()}</h5>
+                <Button variant="contained" onClick={props.localUpdate}>Click to purchase</Button>
+            </div>
+
+            <div className="item">
+                <h3>Jewelry Piece Name</h3>
+                <img src="" alt=""/>
+                <h5>${randomPriceGenerator()}</h5>
+                <Button variant="contained" onClick={props.localUpdate}>Click to purchase</Button>
+            </div>
+
+            <div className="item">
+                <h3>Jewelry Piece Name</h3>
+                <img src="" alt=""/>
+                <h5>${randomPriceGenerator()}</h5>
+                <Button variant="contained" onClick={props.localUpdate}>Click to purchase</Button>
+            </div>
+
+            <div className="item">
+                <h3>Jewelry Piece Name</h3>
+                <img src="" alt=""/>
+                <h5>${randomPriceGenerator()}</h5>
+                <Button variant="contained" onClick={props.localUpdate}>Click to purchase</Button>
+            </div>
+    </>
     )
 }
 

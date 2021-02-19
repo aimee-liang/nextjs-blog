ed139f1fe0123b43d31bc7cb4c5ee8c5ba9fc8a6
aimee-liang <aimeeeliang@gmail.com>
2021-02-18 22:37:08 -0500

    Updated Post() in [id]

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -9,20 +9,22 @@ export async function getStaticPaths() {
     }
   }
 
-export default function Post({postData}) {
-  return (
-    <Layout>
+export default function Post({ postData }) {
+    return (
+      <Layout>
         {postData.title}
         <br />
         {postData.id}
         <br />
         {postData.date}
-    </Layout>
+        <br />
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </Layout>
     )
 }
 
 export async function getStaticProps({params}){
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
